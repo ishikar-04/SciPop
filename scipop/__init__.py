8d9f34e2d9bd2ae3c495747086382629f7199768
Qiyao Liu <qiyaol@uw.edu>
2022-12-07 15:33:14 -0800

    update

--- scipop/__init__.py
+++
@@ -1,1 +0,0 @@
-from .scipop import scraper
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,1 @@
-from scipop.scraper import main
-from scipop.scraper import key_words
-from scipop.scraper import scraping_author
-from scipop.scraper import scraping_doi
+#
